862635a6f19dcb2918c710ff9c9c01b2fd2cbb9a
dianlujitao <dianlujitao@gmail.com>
2017-01-07 22:27:33 -0500

    cherry: Rebrand to MoKee

--- mokee.mk
+++ mokee.mk
@@ -19,11 +19,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/huawei/cherry/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MoKee stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mk.maintainer=dianlujitao
 
 PRODUCT_DEVICE := cherry
-PRODUCT_NAME := lineage_cherry
+PRODUCT_NAME := mk_cherry
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := cherry
 PRODUCT_MANUFACTURER := HUAWEI
